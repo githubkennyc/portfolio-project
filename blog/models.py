599ac408729ac14803d66f3faa77aa604d121409
githubkennyc <11365ken1@gmail.com>
2018-06-25 08:17:25 -0400

    after a day

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=300, blank=True)
+    image = models.ImageField(upload_to="image/")
+    text_body = models.TextField(blank=True)
+    pub_date = models.DateTimeField(default=datetime.now, blank=True)
