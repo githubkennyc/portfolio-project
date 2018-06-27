9e2eeffe36ae6f024a02cbdb727901bad4614198
githubkennyc <11365ken1@gmail.com>
2018-06-27 09:11:43 -0400

    another day

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,12 @@ class Blog(models.Model):
     image = models.ImageField(upload_to="image/")
     text_body = models.TextField(blank=True)
     pub_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.text_body[:100]
+
+    def pretty_time(self):
+        return self.pub_date.strftime('%b %e %Y')
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
 # Create your views here.
+def allblogs(request):
+    allblogs = Blog.objects
+    return render(request, 'blog/allblogs.html', {'allblogs':allblogs})
+
+def blog_detail(request, blog_id):
+    blogdetail=get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/blog_detail.html', {'blog': blogdetail})
